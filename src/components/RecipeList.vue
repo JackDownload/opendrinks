58ba63627a379bf3846d34114248a71cde674e3a
JackDownload <kismocsy@gmail.com>
2021-10-30 23:39:55 +0200

    Update RecipeList.vue

--- src/components/RecipeList.vue
+++ src/components/RecipeList.vue
@@ -49,6 +49,13 @@
       </div>
     </b-card-group>
         <div v-for="(o, i) in paginatedItems" v-bind:key="i" class="card-wrapper mb-2">
+          <figure class="snip1581"><img v-bind:src="o.image ? require(`@/assets/recipes/${o.image}`) : null" alt="o.name"/>
+  <figcaption>
+    <h3 class="title1">{{ o.name }}</h3>
+    <h3 class="title2">Winter</h3>
+    <h3 class="title3">{{ o.description }}</h3>
+  </figcaption><b-button :to="'/recipe/' + o.filename" variant="primary" v-t="'View Recipe'" /></a>
+</figure>
     <div class="w-full sm:w-1/2 md:w-1/3 flex flex-col p-3">
     <div class="bg-white rounded-lg shadow-lg overflow-hidden flex-1 flex flex-col">
               <b-card
